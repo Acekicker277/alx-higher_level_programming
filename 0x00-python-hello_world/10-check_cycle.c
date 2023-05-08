09926d464696f700c732d58afa24e1affc1a2a1e
Acekicker277 <oyetolaayo@gmail.com>
2023-05-08 15:38:24 -0700

    python

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,23 +1,31 @@
 #include "lists.h"
-
 /**
- * check_cycle - checks if a linked list is circular or not
- * @list: linked list to check
- * Return: 1 (linked list is circular) 0 (no loop detected)
- */
+ * check_cycle - Check if a linked list is a cicle
+ * @list: linked list
+ * Return: If the linked is, 1 and if is 0
+ **/
 int check_cycle(listint_t *list)
 {
-	listint_t *s1 = NULL, *s2 = NULL;
+	listint_t *low = NULL, *run = NULL;
+	int count = 0;
 
-	s1 = s2 = list;
-	while (list && s1 && s2 && s1->next && s2->next)
+	low = run = list;
+
+	while (low != NULL)
 	{
-		s1 = s1->next;
-		s2 = s2->next->next;
-		if (!s2 || !s1)
-			return (0);
-		if (s2->next == s1)
-			return (1); 
+		low = low->next;
+		count++;
+
+		if (count == 3)
+		{
+			run = run->next;
+			count = 0;
+		}
+
+		if (low == run)
+		{
+			return (1);
+		}
 	}
 	return (0);
 }
